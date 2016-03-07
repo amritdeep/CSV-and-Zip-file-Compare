f5ad302b1ca5f90b5b1d4e62d59a1321b6b4e7f9
Amrit Deep <amrit@cratebind.com>
2016-03-07 16:06:47 -0600

    Handle the csv upload

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -4,4 +4,20 @@ class Dataset < ActiveRecord::Base
   	has_attached_file :file
   	validates_attachment :file, presence: true,
     :content_type => { content_type: 'text/csv' }
+
+    before_save :parse_file
+
+	  def parse_file
+	    tempfile = file.queued_for_write[:original].read
+	    my_csv = CSV.new(tempfile, :headers => true,
+	                               :header_converters => :symbol,
+	                               :converters => :all)
+	    my_array_of_hashes = my_csv.to_a.map {|row| row.to_hash}
+	    
+	    my_array_of_hashes.each do |hash|
+	      record = self.records.build
+	      record.data = hash
+	      record.save
+	    end
+	  end
 end
