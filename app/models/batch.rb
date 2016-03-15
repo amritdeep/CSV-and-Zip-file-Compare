2c8aafe638463b4743999f980ea1c2f2e8dbf4dd
Amrit Deep <amrit@cratebind.com>
2016-03-15 17:35:31 -0500

    Compare pdf file with csv and remove files after uploaded to s3 server

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -21,36 +21,30 @@ class Batch < ActiveRecord::Base
     :content_type => { content_type: 'application/zip' }
 
     before_save :parse_csv_file
-    after_save :parse_pdf
+    after_save :parse_pdf, :get_pdf_files, :remove_folder
 
     def working_dir
     	"tmp/keystone/"
     end
 
     def extract_dir
-      	folder_name = zipfile_file_name.split('.').first
-    	"#{working_dir}#{folder_name}"  	
+      	@folder_name = zipfile_file_name.split('.').first
+    	"#{working_dir}#{@folder_name}/"  	
     end
 
 	def parse_csv_file
 		tempfile = csvfile.queued_for_write[:original].read
-		cp_csv_file = csvfile.instance.csvfile_file_name
-		File.open("#{working_dir}#{cp_csv_file}", 'w') {|f| f.write(tempfile) }
-		data = SmarterCSV.process("#{working_dir}tempfile.csv")
-		# data.each { |hash| self.records.create(name: hash[:name], pid: hash[:pid])}
+		@cp_csv_file = csvfile.instance.csvfile_file_name
+		File.open("#{working_dir}#{@cp_csv_file}", 'w') {|f| f.write(tempfile) }
+		data = SmarterCSV.process("#{working_dir}#{@cp_csv_file}")
 		data.each { |hash| self.records.build(name: hash[:name], pid: hash[:pid])}
 	end
 
 	def parse_pdf
-		zipfile_file_name = zipfile.instance.zipfile_file_name
-		copy_zip_file =  zipfile.copy_to_local_file(:original, "#{working_dir}#{zipfile_file_name}")
-		local_zip_file = "#{working_dir}#{zipfile_file_name}"
-		# folder_name = zipfile_file_name.split('.').first
-		# binding.pry
+		@zipfile_file_name = zipfile.instance.zipfile_file_name
+		copy_zip_file =  zipfile.copy_to_local_file(:original, "#{working_dir}#{@zipfile_file_name}")
+		local_zip_file = "#{working_dir}#{@zipfile_file_name}"
 		unzip_file(local_zip_file)
-		# upload_to_s3(folder_name)
-		# remove_unwanted_files(local_zip_file, folder_name)
-		remove_folder
 	end
 
 	## 
@@ -62,32 +56,34 @@ class Batch < ActiveRecord::Base
 		end		  	
 	end
 
-	## Uploading to S3 Server
-	def upload_to_s3(folder_name)
-		folder = folder_name.gsub("_", " ")
-		files = Dir.glob("tmp/#{folder_name}/#{folder}/*")
+
+	## Getting the Pdf files from Local Directory
+	def get_pdf_files
+		folder = @folder_name.gsub("_", " ")
+		folder_name = extract_dir + "#{folder}"
+		files = Dir.glob("#{folder_name}/*")
 		store_pdf_file(files)
 	end
 
 	## Store pdf files for record
 	def store_pdf_file(files)
 		self.records.each do |record|
-	  	files.each do |file|
-	  		if file.split('-').last.include?record.data[:name] #|| record.data[:ajb_corp_dbp].include?file.split('-').last
-		  		f = File.open(file)
-		  		record.pdf = f
-		  		f.close
-		  		record.save!
-	  		end
-	  	end
+		  	files.each do |file|
+		  		if file.split('-').last.include?record.name #|| record.data[:ajb_corp_dbp].include?file.split('-').last
+			  		f = File.open(file)
+			  		record.pdf = f
+			  		f.close
+			  		record.save!
+		  		end
+		  	end
 		end	  	
 	end
 
 	## Removing File from Tmp Folder
-	def remove_folder
-		# FileUtils.rm_rf("#{files}")
-		# FileUtils.rm_rf(Dir.glob("tmp/#{folder}"))
-		FileUtils.rm_rf("#{working_dir}")	  	
+	def remove_folder  	
+		FileUtils.rm_rf("#{extract_dir}")
+		FileUtils.rm_rf("#{working_dir}#{@cp_csv_file}")
+		FileUtils.rm_rf("#{working_dir}#{@zipfile_file_name}")	  	
 	end	  
 
 end
