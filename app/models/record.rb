40a79cc2076d280f867d45b9393b12334f471985
Amrit Deep <amrit@cratebind.com>
2016-03-15 17:36:27 -0500

    Format the code

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,19 +1,19 @@
 class Record < ActiveRecord::Base
   belongs_to :dataset
-  # serialize :data, Hash
 
-  	has_attached_file :pdf, 
-  					  default_url: ":attachment/:id/:style.:extension",
-	  				:s3_domain_url => "******.s3.amazonaws.com",
-	                :storage => :s3,
-	                :s3_credentials => Rails.root.join("config/aws.yml"),
-	                :bucket => 'keystone-development',
-	                # :s3_permissions => :public_read,
-	                :s3_permissions => :private,
-	                :s3_protocol => "http",
-	                :convert_options => { :all => "-auto-orient" },
-	                :encode => 'utf8'
+	has_attached_file :pdf, 
+					  default_url: ":attachment/:id/:style.:extension",
+					:s3_domain_url => "******.s3.amazonaws.com",
+	            :storage => :s3,
+	            :s3_credentials => Rails.root.join("config/aws.yml"),
+	            :bucket => 'keystone-development',
+	            # :s3_permissions => :public_read,
+	            :s3_permissions => :private,
+	            :s3_protocol => "http",
+	            :convert_options => { :all => "-auto-orient" },
+	            :encode => 'utf8'
 
-  	validates_attachment :pdf, #presence: true,
-    :content_type => { content_type: 'application/pdf' }
+	validates_attachment :pdf, #presence: true,
+	:content_type => { content_type: 'application/pdf' }
+	
 end
