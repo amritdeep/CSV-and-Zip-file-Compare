792215d5e8f7a40d13d5cb9e17c7e3d83b7ef8f1
Amrit Deep <amrit@cratebind.com>
2016-03-15 11:39:23 -0500

    Change Dataset to Batch

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -2,19 +2,19 @@ class RecordController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-  	@batch = Dataset.find(params[:dataset_id])
+  	@batch = Batch.find(params[:batch_id])
   	@record = Record.find(params[:id])
   	data = open(@record.pdf.expiring_url)
   	send_data data.read, :type => data.content_type, :x_sendfile => true, :url_based_filename => true
   end
 
   def edit
-  	@batch = Dataset.find(params[:dataset_id])
+  	@batch = Batch.find(params[:batch_id])
   	@record = @batch.records.find(params[:id])
   end
 
   def update
-  	@batch = Dataset.find(params[:dataset_id])
+  	@batch = Batch.find(params[:batch_id])
   	@record = @batch.records.find(params[:id])
   	@record.pdf = params[:record][:pdf]
 
