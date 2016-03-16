f3fb870b177779043d42af5e776e650e774ae0e9
Amrit Deep <amrit@cratebind.com>
2016-03-16 13:23:54 -0500

    Test for User assoication and validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+	## Assoication
+	it { should have_many(:batches) }
 
+	## Validation
+	it { should validate_presence_of(:email) }
+	it { should validate_presence_of(:password) }
+	
 end
