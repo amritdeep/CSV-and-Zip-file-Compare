7ad9be4b7bfbcf88563720981ea356c08cbd5098
Amrit Deep <amrit@cratebind.com>
2016-03-16 10:59:14 -0500

    initial tests for app

--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Batch, type: :model do
 
-	# let!(:batch) { create(:batch) }
-	## Validation
-	# it { should validate_presence_of(:name) }
-	it "should validate the present of name" do
-		binding.pry
-		
-		batch = Batch.create(name: "Batch 1")
-	end
+	# let(:batch) { create(:batch) }
+	
+	# Validation
+	it { should validate_presence_of(:name) }
+	
+	# it "should validate the present of name" do
+		# expect(batch.valid?).to eq(true)
+	# end
 
 end
